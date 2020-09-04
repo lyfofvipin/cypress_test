01e7b20743f44872ce313f104c29b4e55f0ba494
kumar vipin yadav <kumarvipinyadav369@gmail.com>
2020-09-04 18:28:18 +0530

    Update vipin.spec.js

--- cypress/integration/examples/vipin.spec.js
+++ cypress/integration/examples/vipin.spec.js
@@ -1,7 +1,7 @@
 const { cyan } = require("color-name")
 
-describe("Test suit", function(){
-    it('test 1', function(){
+describe("My Test suit", function(){
+    it('test_case 1', function(){
         cy.visit("https://github.com")
         cy.title().should('eq', 'The world’s leading software development platform · GitHub')
     })
